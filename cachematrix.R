bb83ec123a2e4579ddcda4c40578926343ca5fdb
ksravana <ksravana@yahoo.com>
2015-01-25 23:41:08 -0800

    cleaned up comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 ## Function calculates inverse of a matrix. If the inverse is already in memory, 
-## it is returned. If not, inverse is computed from "solve" function", the value
-## stored and then it is returned.
-
-## Write a short comment describing this function
+## it is returned. If not, inverse is computed using the "solve" function", its 
+## value stored and then returned.
 
 makeCacheMatrix <- function(x = matrix()) {
     
@@ -23,9 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function calculates invesrse using "solve" function. If the inverse matrix
-## is already in memory, it simply returns it. If not, the function computed the 
-## inverse using the "solve" function and stores it in memory and then return it.
+## This function calculates invesrse using the "solve" function. If the inverse 
+## matrix is already in memory, it simply returns it. If not, the function 
+## computes the inverse using the "solve" function, stores it in memory and then 
+## returns it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
